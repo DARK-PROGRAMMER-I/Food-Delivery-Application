cabe346230d791f11ef9c12792fe46368d9496a1
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2022-10-16 13:12:51 +0500

    Recommended food details

--- lib/pages/food/recommended_food_details.dart
+++ lib/pages/food/recommended_food_details.dart
@@ -3,7 +3,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food_delivery_app/utils/colors.dart';
 import 'package:food_delivery_app/utils/dimensions.dart';
 import 'package:food_delivery_app/widgets/icons_back.dart';
+import 'package:food_delivery_app/widgets/small_text.dart';
 
+import '../../widgets/big_text.dart';
 import '../../widgets/expandable_text.dart';
 
 class RecommendedFoodDetails extends StatelessWidget {
@@ -66,6 +68,57 @@ class RecommendedFoodDetails extends StatelessWidget {
             ),
           )
         ],
+      ),
+      bottomNavigationBar: Container(
+        height: 180.h,
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 20.h),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconsBack(icon: Icons.remove, backgroundColor: Colors.black54),
+                  BigText(name: '\$5677 X 1'),
+                  IconsBack(icon: Icons.add, backgroundColor: Colors.black54),
+                ],
+              ),
+            ),
+
+            Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+                  decoration: BoxDecoration(
+                      color: Colors.black12,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.r),
+                      topRight: Radius.circular(30.r),
+                    )
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconsBack(
+                          icon: Icons.favorite,
+                          iconColor: AppColors.mainColor,
+                          radius: 15.r,
+                          back_size: 50.h,
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+                        decoration: BoxDecoration(
+                          color: AppColors.mainColor,
+                          borderRadius: BorderRadius.circular(15.r)
+                        ),
+                        child: BigText(name: '\$28 | Add to cart', color: AppColors.whiteColor,),
+                      )
+                    ],
+                  ),
+                ),
+            )
+          ],
+        ),
       ),
     );
   }
